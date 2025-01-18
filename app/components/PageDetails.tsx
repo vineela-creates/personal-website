6f46abfef4b2bca3c11b960d89f1c421a4e6d2d5
vineela-creates <vineelalearns@gmail.com>
2025-01-18 16:49:22 +0530

    Update PageDetails.tsx

--- app/components/PageDetails.tsx
+++ app/components/PageDetails.tsx
@@ -27,11 +27,6 @@ const PageDetails: React.FC = () => {
             className="
        scroll-mt-16 md:mb-24 lg:mb-20 lg:scroll-mt-24"
           >
-            <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-              <h1 className="text-sm font-bold uppercase tracking-widest text-blue-800 lg:sr-only">
-                INTRO
-              </h1>
-            </div>
             <div className="mb-4 text-justify">
               <p className="mb-4">
                 I'm a full stack developer passionate about designing and
